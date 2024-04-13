2b22f43d338e78568769e637d7826d77eb432e82
Uianes <biondouianes@gmail.com>
2024-04-13 17:28:47 -0300

    I'm with you always, always

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+var url = window.location.href;
+var lastIndex = url.lastIndexOf("/");
+var pageNumber = url.substring(lastIndex + 1);
+var numero = parseInt(pageNumber) + 1;
+var numero2 = parseInt(pageNumber) - 1;
+var proximaUrl = url.substring(0, lastIndex + 1) + numero + ".html";
+var anteriorUrl = url.substring(0, lastIndex + 1) + numero2 + ".html";
+document.getElementById("prox").href = proximaUrl;
+document.getElementById("ant").href = anteriorUrl;
