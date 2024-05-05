169e4dc110e03cd84c2ee012697974c0c37025fc
Uianes <biondouianes@gmail.com>
2024-05-05 15:13:20 -0300

    I'm with you always

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,17 +2,21 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "../mysql-por-uianes/menu.html", true);
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-        var responseText = xhr.responseText;
-        document.getElementById("menu-placeholder").innerHTML = responseText;
+        document.getElementById("menu-placeholder").innerHTML = xhr.responseText;
 
         var url = window.location.href;
-        var lastIndex = url.lastIndexOf("/");
-        var pageNumber = parseInt(url.substring(lastIndex + 1));
-        var proximaUrl = url.substring(0, lastIndex + 1) + (pageNumber + 1) + ".html";
-        var anteriorUrl = url.substring(0, lastIndex + 1) + (pageNumber - 1) + ".html";
-
-        document.getElementById("prox").href = proximaUrl;
-        document.getElementById("ant").href = (pageNumber == 0) ? "index.html" : anteriorUrl;
+        var lastIndex = url.lastIndexOf("/")
+        var pageNumber = url.substring(lastIndex + 1)
+        var numero = parseInt(pageNumber) + 1
+        var numero2 = parseInt(pageNumber) - 1
+        var proximaUrl = url.substring(0, lastIndex + 1) + numero + ".html"
+        var anteriorUrl = url.substring(0, lastIndex + 1) + numero2 + ".html"
+        document.getElementById("prox").href = proximaUrl
+        if (numero2 == 0) {
+            document.getElementById("ant").href = "index.html"
+        } else {
+            document.getElementById("ant").href = anteriorUrl
+        }
     }
 };
 xhr.send();
