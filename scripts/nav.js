9166f00a53f8bdb9c5f9f9aa853a650f91d1f7e5
Uianes <biondouianes@gmail.com>
2024-05-08 19:56:17 -0300

    Always

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,22 +1,13 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "../mysql-por-uianes/menu.html", true);
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById("menu-placeholder").innerHTML = xhr.responseText;
-
-        var url = window.location.href;
-        var lastIndex = url.lastIndexOf("/")
-        var pageNumber = url.substring(lastIndex + 1)
-        var numero = parseInt(pageNumber) + 1
-        var numero2 = parseInt(pageNumber) - 1
-        var proximaUrl = url.substring(0, lastIndex + 1) + numero + ".html"
-        var anteriorUrl = url.substring(0, lastIndex + 1) + numero2 + ".html"
-        document.getElementById("prox").href = proximaUrl
-        if (numero2 == 0) {
-            document.getElementById("ant").href = "index.html"
-        } else {
-            document.getElementById("ant").href = anteriorUrl
-        }
-    }
-};
-xhr.send();
+var url = window.location.href;
+var lastIndex = url.lastIndexOf("/")
+var pageNumber = url.substring(lastIndex + 1)
+var numero = parseInt(pageNumber) + 1
+var numero2 = parseInt(pageNumber) - 1
+var proximaUrl = url.substring(0, lastIndex + 1) + numero + ".html"
+var anteriorUrl = url.substring(0, lastIndex + 1) + numero2 + ".html"
+document.getElementById("prox").href = proximaUrl
+if (numero2 == 0) {
+    document.getElementById("ant").href = "index.html"
+} else {
+    document.getElementById("ant").href = anteriorUrl
+}
